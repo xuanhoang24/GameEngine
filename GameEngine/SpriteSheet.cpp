0eca6c1062467abe089ce0378f15380532e7e084
Xuan Hoang Ha <hoanghaxuan2434@gmail.com>
2025-02-20 14:32:58 -0500

    Fix-bug: W6-10

--- GameEngine/SpriteSheet.cpp
+++ GameEngine/SpriteSheet.cpp
@@ -34,7 +34,7 @@ Rect SpriteSheet::Update(AnimationNames _name, float _deltaTime)
 {
 	short s = m_animations[_name]->GetClipCurrent();
 	short posX = s % m_columns * m_clipSizeX;
-	short posY = s % m_columns * m_clipSizeY;
+	short posY = s / m_columns * m_clipSizeY;
 	Rect r = Rect(posX, posY, posX + m_clipSizeX, posY + m_clipSizeY);
 
 	m_animations[_name]->Update(_deltaTime);
